a714026cccd61babff9d5e39da741232c5828c45
boronkay-tanulo <116671355+boronkay-tanulo@users.noreply.github.com>
2023-01-10 20:01:48 +0100

    Add files via upload

---
+++ kapu_script.js
@@ -0,0 +1,8 @@
+function General()
+{
+    var kep = document.createElement("img"); // deklaráltam egy absztrakt kép változót
+    kep.setAttribute("src", "kepek/0.png");
+    kep.setAttribute("width", "150");
+    kep.setAttribute("height", "150");
+    document.getElementById("d0").appendChild(kep);
+}
